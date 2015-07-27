358e397b3ddf4db5a7cf4c70f4ff9c46944b3b86
David East <david@firebase.com>
2015-07-27 11:49:49 -0700

    TitleLabel

---
+++ TappyBase/TitleLabel.swift
@@ -0,0 +1,26 @@
+//
+//  TitleLabel.swift
+//  TappyBase
+//
+//  Created by deast on 7/27/15.
+//  Copyright (c) 2015 davideast. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class TitleLabel: SKSpriteNode {
+  
+  init() {
+    let texture = SKTexture(imageNamed: TappyBaseImages.titleImage())
+    super.init(texture: texture, color: nil, size: texture.size())
+    name = TappyBaseImages.titleImage()
+    zPosition = CGFloat(1)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  
+}
